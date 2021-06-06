ab795d76426ac02e9391d2c4c2e94f0f666d3fb5
pratyaydeep <pratyaydeepghanta@gmail.com>
2021-06-06 12:14:32 +0530

    Prewritten Local code being comitted

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from .extensions import db
+from .routes import short
+
+def create_app(config_file="settings.py"):
+    app= Flask(__name__)
+    app.config.from_pyfile(config_file)
+    db.init_app(app)
+    app.register_blueprint(short)
+    return app
+
+
+
+#start pipenv virtual environment before anything alse by
+#   move to the root-most folder of the project
+#   type in command prompt
+#   pipenv shell
+#   good to go
+
+#creating database before forst use
+#   from url_shortner import 
